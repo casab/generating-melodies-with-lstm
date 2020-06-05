6516299ab3f67fde815ac065db6109bcc10eafdc
Engin Al <enginal@gmail.com>
2020-06-05 23:10:01 +0300

    Added TODOs for preprocessing the data

---
+++ preprocess.py
@@ -0,0 +1,12 @@
+def preprocess(dataset_path):
+    pass
+
+    # TODO: load the folk songs
+
+    # TODO: filter out songs that have non-acceptable durations
+
+    # TODO: transpose songs to Cmaj/Amin
+
+    # TODO: encode songs with music time series represantation
+
+    # TODO: save songs to text file
